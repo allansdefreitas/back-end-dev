ba79ba015abbad47422a368dcba24fd942618125
Allan Freitas <allan.freitas@serpro.gov.br>
2025-05-08 16:40:34 -0300

    exercício

--- c#/learn/alura/1-C# - criando sua primeira aplicação/SalveMundus/Program.cs
+++ c#/learn/alura/1-C# - criando sua primeira aplicação/SalveMundus/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string welcomeMessage = "Welcome to Screen Sound";
+Console.WriteLine(welcomeMessage);
+
+
+string curso = "C#: Criando sua primeira aplicação C#";
+string nome = "Guilherme Lima & Daniel Portugal";
+
+Console.WriteLine("O nome do curso é : " + curso);
+Console.WriteLine("Os nomes dos instrutores são: " + nome);
+
