d505a8cf1a1425496f4081d10e16398f60db35ef
Allan Freitas <allan.freitas@serpro.gov.br>
2025-05-13 11:35:19 -0300

    Average of a band

--- c#/learn/alura/1-C# - criando sua primeira aplicação/ScreenSound/Program.cs
+++ c#/learn/alura/1-C# - criando sua primeira aplicação/ScreenSound/Program.cs
@@ -3,10 +3,12 @@
 string welcomeMessage = "Welcome to Screen Sound!";
 //List<string> bandsList = new List<string> { "Arautos do Rei", "Prisma Brasil", "Banda e Orquestra Canaã" };
 
-Dictionary<string, List<int>> bandsAndRatingsDict = new Dictionary<string, List<int>>();
-bandsAndRatingsDict.Add("Voices", new List<int> { 10, 9, 7, 9 });
-bandsAndRatingsDict.Add("Arautos do Rei", new List<int>());
-bandsAndRatingsDict.Add("Musical Formosa", new List<int>());
+Dictionary<string, List<int>> bandsAndRatingsDict = new Dictionary<string, List<int>>
+{
+    {"Voices", new List<int> { 10, 7, 9, 8}},
+    {"Arautos do Rei", new List<int> { 10, 7, 9, 8}},
+    {"Musical Formosa", new List<int> ()}
+};
 
 void ShowLogo()
 {
@@ -31,6 +33,7 @@ void ShowMainMenu()
     Console.WriteLine("Type 2 to show all bands");
     Console.WriteLine("Type 3 to rate a band");
     Console.WriteLine("Type 4 to show the average of a band");
+    Console.WriteLine("Type 5 to show the ratings of all bands");
     Console.WriteLine("Type -1 to exit");
 
     Console.Write("\nType an option: ");
@@ -47,10 +50,14 @@ void ShowMainMenu()
             ListBands();
             break;
         case 3:
-            Console.WriteLine("You chose option " + chosenOption);
+            RateABand();
             break;
         case 4:
-            Console.WriteLine("You chose option " + chosenOption);
+            ListAverageRatingOfABand();
+            break;
+
+        case 5:
+            ListAllBandRatings();
             break;
         case -1:
             Console.WriteLine("You chose option " + chosenOption);
@@ -112,6 +119,92 @@ void ListBands()
     Console.ReadKey();
     Console.Clear();
     ShowMainMenu();
+}
+
+void RateABand()
+{
+    Console.Clear();
+    ShowMenuTitle("Rate bands");
+    Console.Write("Type the name of the band you want to rate: ");
+    string bandName = Console.ReadLine()!;
+
+    if (bandsAndRatingsDict.ContainsKey(bandName))
+    {
+        Console.Write("Type the rate of the band: ");
+        string bandRate = Console.ReadLine()!;
+        int bandRateInt = int.Parse(bandRate);
+
+        bandsAndRatingsDict[bandName].Add(bandRateInt);
+        Console.WriteLine($"The band '{bandName}'was rated successfully!");
+        Thread.Sleep(2000);
+        Console.Clear();
+        ShowMainMenu();
+    }
+    else
+    {
+        Console.WriteLine($"The band '{bandName}' does was not found!");
+        Console.WriteLine("\nType any key to back to main menu");
+        Console.ReadKey();
+        Console.Clear();
+        ShowMainMenu();
+    }
+}
+
+void ListAverageRatingOfABand()
+{
+    Console.Clear();
+    ShowMenuTitle("List average rating of a band");
+    Console.Write("Type the name of a band: ");
+    string bandName = Console.ReadLine()!;
+
+    if (bandsAndRatingsDict.ContainsKey(bandName))
+    {
+
+
+        List<int> ratingsList = bandsAndRatingsDict[bandName];
+        double bandAverageRating = ratingsList.Average();
+
+        Console.WriteLine($"The average of band '{bandName}' is: {bandAverageRating}");
+
+    }
+    else
+    {
+        Console.WriteLine($"The band '{bandName}' does not exist!");
+    }
+
+
+
+
+
+
+}
+
+
+void ListAllBandRatings()
+{
+    Console.Clear();
+    ShowMenuTitle("List band ratings");
+
+    foreach (KeyValuePair<string, List<int>> kvpBand in bandsAndRatingsDict)
+    {
+        Console.WriteLine("Band: {0}", kvpBand.Key);
+
+        Console.Write("Ratings: ");
+        if (kvpBand.Value.Count > 0)
+        {
+
+            foreach (int rating in kvpBand.Value)
+            {
+                Console.Write("{0} ", rating);
+            }
+            Console.Write("\n\n");
+        }
+        else
+        {
+            Console.WriteLine("The band has not ratings yet!");
+        }
+    }
+
 }
 
 ShowMainMenu();
