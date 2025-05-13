1fd68963a20564d442c3cd17887d24aa4e5ad0b6
Allan Freitas <allan.freitas@serpro.gov.br>
2025-05-13 15:03:56 -0300

    exercício

---
+++ c#/learn/alura/1-C# - criando sua primeira aplicação/EstoqueLoja/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+Console.WriteLine("Sistema de controle de estoque");
+
+Dictionary<string, int> produtosQuantidades = new Dictionary<string, int> {
+    {"Máquina de lavar louça 14 serviços", 1},
+    {"Freezer vertical", 1},
+    {"Sugar", 1},
+    {"Ar condicionado", 1},
+    {"Fogão 6 bocas", 1},
+    {"Air Fryer", 1},
+    {"Micro Ondas", 1},
+    {"Estante de livros", 2}
+};
+
+
+Console.Write("Digite o nome do produto: ");
+string nomeProduto = Console.ReadLine()!;
+
+if (produtosQuantidades.Keys.Contains(nomeProduto))
+{
+    int quantidade = produtosQuantidades[nomeProduto]!;
+    Console.WriteLine($"Quantidade: {quantidade}");
+}
+else
+{
+    Console.WriteLine("O produto não existe!");
+}
+
+
