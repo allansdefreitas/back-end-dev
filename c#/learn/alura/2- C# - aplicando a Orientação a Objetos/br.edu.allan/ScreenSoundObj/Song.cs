24357e098ded3bdf7e1f0af616b1c89ff1b7804c
allansdefreitas <allan.allan.santos@gmail.com>
2025-05-14 11:34:21 -0300

    lambda

--- c#/learn/alura/2- C# - aplicando a Orientação a Objetos/br.edu.allan/ScreenSoundObj/Song.cs
+++ c#/learn/alura/2- C# - aplicando a Orientação a Objetos/br.edu.allan/ScreenSoundObj/Song.cs
@@ -4,6 +4,11 @@ class Song
     public string Artist {get; set;}
     public int Duration {get; set;}
     public bool IsAvailableInPlann {get; set;}
+
+    // short version. read only. Called arrow function or Lambda in C#
+    //public string SummarizedDescription => 
+    //    $"The song \"{Title}\" belongs to the artist \"{Artist}\" and has a duration of about {Duration} minutes";
+
     public string SummarizedDescription { 
         
         get{
