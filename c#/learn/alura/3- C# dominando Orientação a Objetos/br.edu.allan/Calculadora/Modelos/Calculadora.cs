9e852bfb46adc0951e30b7ae6a7c102c4b6a64b1
allansdefreitas <allan.allan.santos@gmail.com>
2025-05-21 11:02:59 -0300

    Calculadora

---
+++ c#/learn/alura/3- C# dominando Orientação a Objetos/br.edu.allan/Calculadora/Modelos/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿namespace Edu.Allan.Calculadora.Modelos
+
+{
+    public class Calculadora
+    {
+
+        public static double Somar(double valor1, double valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public static double Subtrair(double valor1, double valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public static double Multiplicar(double valor1, double valor2)
+        {
+            return valor1 * valor2;
+        }
+
+        public static double Dividir(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+
+        public static double RaizQuadrada(double valor)
+        {
+            return Math.Sqrt(valor);
+        }
+
+        public static double Potencia(double valorBase, double valorExpoente)
+        {
+            return Math.Pow(valorBase, valorExpoente);
+        }
+    }
+}
---
+++ c#/learn/alura/3- C# dominando Orientação a Objetos/br.edu.allan/Calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿using Edu.Allan.Calculadora.Modelos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Console.WriteLine(Calculadora.Somar(1, 10));
+
+        Console.WriteLine(Calculadora.Subtrair(10, 9));
+
+        Console.WriteLine(Calculadora.Multiplicar(10, 9));
+
+        Console.WriteLine(Calculadora.Dividir(10, 2));
+
+        Console.WriteLine(Calculadora.RaizQuadrada(144));
+
+        Console.WriteLine(Calculadora.Potencia(10, 2));
+    }
+}
