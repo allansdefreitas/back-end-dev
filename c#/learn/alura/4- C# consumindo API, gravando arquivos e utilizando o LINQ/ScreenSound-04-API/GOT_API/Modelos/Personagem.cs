89f5442d71dd05e74ee6acaa879cee59a471e684
allansdefreitas <allan.allan.santos@gmail.com>
2025-06-17 16:55:07 -0300

    GOT API

---
+++ c#/learn/alura/4- C# consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04-API/GOT_API/Modelos/Personagem.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GOT_API.Modelos;
+
+internal class Personagem
+{
+
+    [JsonPropertyName("name")]
+    public string Nome { get; set; }
+
+    [JsonPropertyName("gender")]
+    public string Sexo { get; set; }
+
+    [JsonPropertyName("born")]
+    public string InfosNascimento { get; set; }
+
+    [JsonPropertyName("titles")]
+    public List<string>? Titulos { get; set; }
+
+
+    public Personagem() { 
+    
+        Titulos = new List<string>();
+    }
+
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Sexo: {Sexo}");
+        Console.WriteLine($"Data e local de nascimento: {InfosNascimento}");
+        Console.WriteLine("Títulos: ");
+        Titulos.ForEach(t => Console.WriteLine($"\"{t}\""));
+            
+    }
+
+
+
+}
---
+++ c#/learn/alura/4- C# consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04-API/GOT_API/Program.cs
@@ -0,0 +1,21 @@
+﻿using GOT_API.Modelos;
+using System.Text.Json;
+
+internal class Program
+{
+    private const string URL_API = "https://www.anapioficeandfire.com/api/characters/16";
+
+    private static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var json = await client.GetStringAsync(URL_API);
+            
+
+            var personagem = JsonSerializer.Deserialize<Personagem>(json)!;
+
+            personagem.ExibirDetalhes();
+
+        }
+    }
+}
