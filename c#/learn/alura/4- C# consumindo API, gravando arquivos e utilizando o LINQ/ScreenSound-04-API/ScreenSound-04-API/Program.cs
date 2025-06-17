8f48e47e0e7d6ea6702ca1efcd6040013ad41eff
allansdefreitas <allan.allan.santos@gmail.com>
2025-06-17 11:38:57 -0300

    Requisição à API com C#

---
+++ c#/learn/alura/4- C# consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04-API/ScreenSound-04-API/Program.cs
@@ -0,0 +1,16 @@
+﻿
+internal class Program
+{
+
+    private const string URL_API_GET = "https://raw.githubusercontent.com/guilhermeonrails/api-csharp-songs/refs/heads/main/songs.json";
+    private static async Task Main(string[] args)
+
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            
+            string resposta = await client.GetStringAsync(URL_API_GET);
+            Console.WriteLine(resposta);
+        }
+    }
+}
