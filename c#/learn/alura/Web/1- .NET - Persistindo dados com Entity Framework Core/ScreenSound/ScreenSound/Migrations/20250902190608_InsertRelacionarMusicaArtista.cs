387dc3c5232acbf15bb306abaa8947b4d7dbbc9c
allansdefreitas <allan.allan.santos@gmail.com>
2025-09-02 16:14:27 -0300

    C#: More Migrations

---
+++ c#/learn/alura/Web/1- .NET - Persistindo dados com Entity Framework Core/ScreenSound/ScreenSound/Migrations/20250902190608_InsertRelacionarMusicaArtista.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertRelacionarMusicaArtista : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Deus Muda Situações → Victorino Silva
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistId = 14 WHERE Id = 9");
+
+            // Cem Ovelhas → Ozéias de Paula
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistId = 19 WHERE Id = 10");
+
+            migrationBuilder.Sql("DELETE FROM Musicas WHERE Id = 11");
+
+            // Eu Não Mudo → Raquel Silva
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistId = 16 WHERE Id = 12");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musicas SET ArtistId = NULL");
+        }
+    }
+}
