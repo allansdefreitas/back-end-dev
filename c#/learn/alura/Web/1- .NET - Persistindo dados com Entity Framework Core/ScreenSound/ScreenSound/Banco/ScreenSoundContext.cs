972970f37fc8132cb85f2438aa7dc1d68079568e
allansdefreitas <allan.allan.santos@gmail.com>
2025-09-02 12:52:46 -0300

    C#: Fixing issues on migrations

--- c#/learn/alura/Web/1- .NET - Persistindo dados com Entity Framework Core/ScreenSound/ScreenSound/Banco/ScreenSoundContext.cs
+++ c#/learn/alura/Web/1- .NET - Persistindo dados com Entity Framework Core/ScreenSound/ScreenSound/Banco/ScreenSoundContext.cs
@@ -1,23 +1,17 @@
-﻿using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco;
 
 internal class ScreenSoundContext: DbContext
 {
-
     public DbSet<Artist> Artists { get; set; }
     public DbSet<Song> Songs { get; set; }
 
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;" +
-        "Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;";
-
+    private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(connectionString);
     }
-
-
 }
